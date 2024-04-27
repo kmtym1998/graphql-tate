f27983452e410c6d7157450c9f4727b08493e25c
kmtym1998 <kmtym.devloper@gmail.com>
2024-04-27 22:42:45 +0900

    graphql server example

--- example/basic/cmd/server/main.go
+++ example/basic/cmd/server/main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/kmtym1998/graphql-tate/example/generated"
+	"github.com/kmtym1998/graphql-tate/example/model"
+	"github.com/kmtym1998/graphql-tate/example/resolver"
+)
+
 func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("POST /v1/graphql", v1postGraphQLHandler())
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := http.ListenAndServe(
+		":"+port,
+		mux,
+	); err != nil {
+		panic(err)
+	}
+}
+
+func v1postGraphQLHandler() http.HandlerFunc {
+	es := generated.NewExecutableSchema(generated.Config{
+		Resolvers: &resolver.Resolver{
+			Todos: []*model.Todo{
+				{ID: "1", Text: "todo1", Done: false},
+				{ID: "2", Text: "todo2", Done: true},
+			},
+		},
+	})
+
+	srv := handler.NewDefaultServer(es)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		srv.ServeHTTP(w, r)
+	})
 }
--- example/basic/resolver/resolver.go
+++ example/basic/resolver/resolver.go
@@ -1,7 +1,11 @@
 package resolver
 
+import "github.com/kmtym1998/graphql-tate/example/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Todos []*model.Todo
+}
