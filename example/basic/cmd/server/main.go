1e58ad9a1e10bf5f7f666c57e68633411d8f3c2b
kmtym1998 <kmtym.devloper@gmail.com>
2024-04-28 10:43:01 +0900

    wip

--- example/basic/cmd/server/main.go
+++ example/basic/cmd/server/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/kmtym1998/graphql-tate/example/generated"
 	"github.com/kmtym1998/graphql-tate/example/model"
 	"github.com/kmtym1998/graphql-tate/example/resolver"
 	"github.com/lmittmann/tint"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 func main() {
@@ -43,16 +47,60 @@ func main() {
 }
 
 func v1postGraphQLHandler() http.HandlerFunc {
+	user1 := &model.User{ID: "U1", Name: "user1"}
+	user2 := &model.User{ID: "U2", Name: "user2"}
+	todo1 := &model.Todo{ID: "1", Text: "todo1", Done: false, User: user1}
+	todo2 := &model.Todo{ID: "2", Text: "todo2", Done: true, User: user2}
+	todo3 := &model.Todo{ID: "3", Text: "todo2", Done: true, User: user2}
+	user1.Todos = []*model.Todo{todo1}
+	user2.Todos = []*model.Todo{todo2, todo3}
+
 	es := generated.NewExecutableSchema(generated.Config{
 		Resolvers: &resolver.Resolver{
+			UserList: []*model.User{
+				user1,
+				user2,
+			},
 			TodoList: []*model.Todo{
-				{ID: "1", Text: "todo1", Done: false},
-				{ID: "2", Text: "todo2", Done: true},
+				todo1,
+				todo2,
+				todo3,
 			},
 		},
 	})
 
 	srv := handler.NewDefaultServer(es)
+
+	srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+		operationCtx := graphql.GetOperationContext(ctx)
+
+		slog.Info(
+			"OperationContext",
+			"operationCtx.Doc.Operations", fmt.Sprintf("%#v", operationCtx.Doc.Operations),
+		)
+
+		var rootFieldNames []string
+		for _, sel := range operationCtx.Doc.Operations[0].SelectionSet {
+			slog.Info(
+				"SelectionSet",
+				"sel", fmt.Sprintf("%#v", sel),
+			)
+
+			f, ok := sel.(*ast.Field)
+			if !ok {
+				continue
+			}
+
+			rootFieldNames = append(rootFieldNames, f.Name)
+		}
+
+		slog.Info(
+			"Responses",
+			"operationCtx", fmt.Sprintf("%#v", operationCtx),
+		)
+
+		return next(ctx)
+	})
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		srv.ServeHTTP(w, r)
