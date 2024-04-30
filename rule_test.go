3ab19fc89a54912edd1b2e510c97d596c1562be5
kmtym1998 <kmtym.devloper@gmail.com>
2024-04-30 23:57:53 +0900

    Add unit tests for validate function

--- rule_test.go
+++ rule_test.go
@@ -146,3 +146,107 @@ func TestNOT(t *testing.T) {
 		})
 	}
 }
+
+type validateTestCase struct {
+	name      string
+	permitter ChildFieldPermission
+	expected  error
+}
+
+func TestValidate(t *testing.T) {
+	for _, tc := range []validateTestCase{
+		{
+			name:      "no rules",
+			permitter: ChildFieldPermission{},
+			expected:  nil,
+		}, {
+			name: "with field permission",
+			permitter: ChildFieldPermission{
+				"field1": Any(),
+				"field2": Any(),
+				"field3": None(),
+			},
+			expected: nil,
+		}, {
+			name: "with field permission but no rules",
+			permitter: ChildFieldPermission{
+				"field1": OR(),
+				"field2": nil,
+				"field3": AND(),
+			},
+			expected: errors.New("invalid permitter type: <nil> field: field2"),
+		}, {
+			name: "with field permission but invalid rule",
+			permitter: ChildFieldPermission{
+				"field1": RuleFunc(nil),
+				"field2": Any(),
+				"field3": AND(),
+			},
+			expected: errors.New("invalid permitter. RuleFunc cannot be nil for field: field1"),
+		}, {
+			name: "with child field permission",
+			permitter: ChildFieldPermission{
+				"field1": ChildFieldPermission{
+					"field1-1": Any(),
+					"field1-2": Any(),
+					"field1-3": None(),
+				},
+				"field2": Any(),
+				"field3": None(),
+			},
+			expected: nil,
+		}, {
+			name: "with child field permission but invalid rule in child field permission",
+			permitter: ChildFieldPermission{
+				"field1": ChildFieldPermission{
+					"field1-1": RuleFunc(nil),
+					"field1-2": Any(),
+					"field1-3": AND(),
+				},
+				"field2": Any(),
+				"field3": None(),
+			},
+			expected: errors.New("child field has invalid permitter field: field1: invalid permitter. RuleFunc cannot be nil for field: field1-1"),
+		}, {
+			name: "with child field permission but invalid rule in parent field permission",
+			permitter: ChildFieldPermission{
+				"field1": ChildFieldPermission{
+					"field1-1": Any(),
+					"field1-2": Any(),
+					"field1-3": None(),
+				},
+				"field2": RuleFunc(nil),
+				"field3": AND(),
+			},
+			expected: errors.New("invalid permitter. RuleFunc cannot be nil for field: field2"),
+		}, {
+			name: "with child field permission but invalid rule in grandchild field permission",
+			permitter: ChildFieldPermission{
+				"field1": ChildFieldPermission{
+					"field1-1": Any(),
+					"field1-2": Any(),
+					"field1-3": AND(),
+				},
+				"field2": ChildFieldPermission{
+					"field2-1": Any(),
+					"field2-2": ChildFieldPermission{
+						"field2-2-1": RuleFunc(nil),
+						"field2-2-2": Any(),
+						"field2-2-3": Any(),
+					},
+					"field2-3": Any(),
+				},
+				"field3": AND(),
+			},
+			expected: errors.New("child field has invalid permitter field: field2: child field has invalid permitter field: field2-2: invalid permitter. RuleFunc cannot be nil for field: field2-2-1"),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.permitter.validate()
+
+			if err != nil && (tc.expected == nil || err.Error() != tc.expected.Error()) {
+				t.Errorf("expected: `%#v`, got: `%#v`", tc.expected, err)
+			}
+		})
+	}
+}
