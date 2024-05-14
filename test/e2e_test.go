0aa42c332dce6641b4dda6354844bfbdeac71932
kmtym1998 <kmtym.devloper@gmail.com>
2024-05-14 21:06:33 +0900

    refactor: Improve code readability by renaming findAvailablePort function

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -140,7 +140,7 @@ func TestE2E(t *testing.T) {
 	})
 }
 
-// 空いているポートを探して返す
+// findAvailablePort finds an available port
 func findAvailablePort() string {
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
